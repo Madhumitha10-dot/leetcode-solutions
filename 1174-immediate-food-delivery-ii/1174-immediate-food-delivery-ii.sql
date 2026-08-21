9b1c5d2a43a8ca0754a23b66a4947b1f356b673a
M Madhumitha <madhumithamuthu10@gmail.com>
2026-08-21 20:54:41 +0530

    Time: 660 ms (66.41%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT ROUND(
+    100*AVG(CASE
+        WHEN order_date=customer_pref_delivery_date THEN 1
+        ELSE 0
+    END),2) AS immediate_percentage
+FROM delivery
+WHERE (customer_id,order_date) IN
+(SELECT customer_id,MIN(order_date)
+FROM delivery
+GROUP BY customer_id);
