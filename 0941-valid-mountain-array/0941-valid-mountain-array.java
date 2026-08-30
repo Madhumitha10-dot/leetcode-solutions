a8f11715d85eb2db5fba0f581fd097371969c10f
M Madhumitha <madhumithamuthu10@gmail.com>
2026-08-30 12:30:26 +0530

    Time: 1 ms (99.94%), Space: 47.8 MB (16.53%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        int i=1;
-        int n=arr.length;
+       int i=1;
+       int n=arr.length;
 
-        if(n<3)
+       if(n<3){
         return false;
-        while(i<n && arr[i]>arr[i-1]){
-            i++;
-        }
-        if(i==1 || i==n){
-            return false;
-        }
-        while(i<n && arr[i]<arr[i-1]){
-            i++;
-        }
-        if(i==n){
-            return true;
-        }else{
-            return false;
-        }
+       }
+       while(i<n && arr[i]>arr[i-1]){
+        i++;
+       }
+       if(i==1 || i==n){
+        return false;
+       }
+       while(i<n && arr[i]<arr[i-1]){
+        i++;
+       }
+       if(i==n){
+        return true;
+       }else{
+        return false;
+       }
     }
 }
